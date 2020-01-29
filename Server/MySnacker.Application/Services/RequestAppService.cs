fbc994288ffaebb4e719a8f3f689bd1360629230
Gumbers <gustavomarinsuppi@gmail.com>
2020-01-28 21:24:52 -0300

    chore: adding application project with automapper

---
+++ Server/MySnacker.Application/Services/RequestAppService.cs
@@ -0,0 +1,31 @@
+﻿using MySnacker.Data.Contexts;
+using MySnacker.Domain.Entities;
+using MySnacker.Domain.Interfaces.ApplicationServices;
+using MySnacker.Domain.Interfaces.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MySnacker.Application.Services
+{
+    public class RequestAppService : IRequestAppService
+    {
+        private readonly IRequestRepository _requestRepository;
+        private readonly DbContext _context;
+
+        public RequestAppService(IRequestRepository requestRepository, SnackerContext context)
+        {
+            _requestRepository = requestRepository;
+
+            _context = context;
+        }
+
+        public void Store(Request request)
+        {
+            using (_context)
+            {
+                _requestRepository.Store(request);
+
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Server/MySnacker.Application/Services/SnackAppService.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MySnacker.Data.Contexts;
+using MySnacker.Domain.Interfaces.ApplicationServices;
+using MySnacker.Domain.Interfaces.Data;
+using MySnacker.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+
+namespace MySnacker.Application.Services
+{
+    public class SnackAppService : ISnackAppService
+    {
+        private readonly ISnackRepository _snackRepository;
+        private readonly SnackerContext _context;
+        private readonly IMapper _mapper;
+
+        public SnackAppService(ISnackRepository snackRepository, SnackerContext context, IMapper mapper)
+        {
+            _snackRepository = snackRepository;
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<SnackListData> GetSnackList()
+        {
+            using (_context)
+            {
+                return _snackRepository.GetSnackList();
+            }
+        }
+
+        public SnackData GetSnackWithData(Guid id)
+        {
+            using (_context)
+            {
+                var snack = _snackRepository.GetSnackWithData(id);
+
+                return _mapper.Map<SnackData>(snack);
+            }
+        }
+    }
+}
